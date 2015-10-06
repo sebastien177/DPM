3770b2a6f8431ab15dfd21d41a2df8dff1a45951
andk123 <awkokoroko@hotmail.com>
2015-10-06 01:49:48 -0400

    Lab3_notCompleted

--- Lab3-EV3-Navigation/src/ev3Navigation/Lab3.java
+++ Lab3-EV3-Navigation/src/ev3Navigation/Lab3.java
@@ -19,30 +19,31 @@ import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.SampleProvider;
 
 public class Lab3 {
-	
+
 	// Static Resources:
 	// Left motor connected to output A
 	// Right motor connected to output D
 	// Ultrasonic sensor connected to input port S1
 	private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
-	private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
+	private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
 	private static final Port usPort = LocalEV3.get().getPort("S1");
 
 	// Constants
 	public static final double WHEEL_RADIUS = 2.17;
 	public static final double TRACK = 15.175;
 
-	
+
+
 	public static void main(String[]args){
 		int buttonChoice;
 		// Instantiating Odometer and Odometer display
-		
+
 		final TextLCD t = LocalEV3.get().getTextLCD();
 		Odometer odometer = new Odometer(leftMotor, rightMotor);
 		Navigation navigation = new Navigation(odometer, leftMotor, rightMotor);
 		OdometryDisplay odometryDisplay = new OdometryDisplay(odometer,t);
 		ObstacleAvoidanceNavigation navigationObstacle = new ObstacleAvoidanceNavigation(odometer, leftMotor, rightMotor);
-		
+
 		// Setup ultrasonic sensor
 		// Note that the EV3 version of leJOS handles sensors a bit differently.
 		// There are 4 steps involved:
@@ -50,14 +51,15 @@ public class Lab3 {
 		// 2. Create a sensor instance and attach to port
 		// 3. Create a sample provider instance for the above and initialize operating mode
 		// 4. Create a buffer for the sensor data
-		
+
 		//@SuppressWarnings("resource")							    // Because we don't bother to close this resource
 		SensorModes usSensor = new EV3UltrasonicSensor(usPort);		// usSensor is the instance
 		SampleProvider usDistance = usSensor.getMode("Distance");	// usDistance provides samples from this instance
 		float[] usData = new float[usDistance.sampleSize()];		// usData is the buffer in which data are returned
-		
+
 		// Setup Ultrasonic Poller									// This thread samples the US and invokes
 		UltrasonicPoller usPoller = null;							// the selected controller on each cycle
+		UltrasonicPoller usPoller2 = null;
 
 		do {
 			// Clear the display
@@ -67,38 +69,32 @@ public class Lab3 {
 			t.drawString("Left, Navigate ", 0, 0);	
 			t.drawString("Right, Obstacle", 0, 1);
 			buttonChoice = Button.waitForAnyPress();
-			
+
 		} 
-		
-		
-		
-		
+
+
 		while (buttonChoice != Button.ID_LEFT
 				&& buttonChoice != Button.ID_RIGHT);
-		
+
 		if (buttonChoice == Button.ID_LEFT) {
 			odometer.start();
 			odometryDisplay.start();
 			navigation.start();
 		}
-		
+
 		else if (buttonChoice == Button.ID_RIGHT) {
 			odometer.start();
 			odometryDisplay.start();
 			usPoller = new UltrasonicPoller(usDistance, usData, navigationObstacle);
-			//navigationObstacle.start();
 			usPoller.start();
+			navigationObstacle.start();
 		}
-		
-		else {
-			// Do something
-		}
-		
+
 		// to end the program
-				while (Button.waitForAnyPress() != Button.ID_ESCAPE);
-				System.exit(0);
-		
-		
+		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+		System.exit(0);
+
+
 	}
 
 }
